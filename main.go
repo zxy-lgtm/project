06c49eb81743681de1a179f5a4e686a66fb19fd7
zxy-lgtm <1563925535qq.com>
2021-05-20 20:16:06 +0800

    create dbinit.go fix main.go

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
+	"project/model"
 	"project/router"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 
+	model.Db.Init()
+	defer model.Db.Close()
+
 	r := gin.Default()
 	router.Router(r)
+	if err := r.Run(); err != nil {
+		log.Fatal(err.Error())
+	}
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
